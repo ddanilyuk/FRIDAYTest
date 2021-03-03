7d8159ab565147857a645ff3aa471d36d23a5375
ddanilyuk <danis.danilyuk@gmail.com>
2021-03-03 16:03:05 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FRIDAY",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "FRIDAY",
